a3433b7167b11daa2c922b8a90df8f44c13f855f
Chanlyn04 <schanlyn25@gmail.com>
2024-10-03 20:05:32 +0800

    I hours OCT3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { PostComponent } from './post/post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ 
+    {path: '', component: WelcomePageComponent},     
+    {path: 'post', component: PostComponent}
+];
